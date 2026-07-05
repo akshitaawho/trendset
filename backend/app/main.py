364625dc6ba9801bc29c9882421b1cea52f05303
akshitaa <163016409+akshitaawho@users.noreply.github.com>
2026-07-05 23:10:57 +0530

    Prepare backend for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,9 @@ app.include_router(dashboard_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
